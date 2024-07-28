04e31798c773467eb6c52fcd5d288f39f161b9ca
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-28 01:41:42 -0400

    created Hero component

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,18 @@
+const Hero = () => {
+  return (
+    <>
+      <section className="mt-72 pb-6">
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-col items-center">
+          <div className="text-center">
+            <h1 className="text-4xl font-extrabold  sm:text-5xl md:text-6xl">
+              Welcome
+            </h1>
+            <p className="text-4xl">Keep it up!</p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
