191e6c162ffd201a6ff8ea07434ec63af24422d7
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-27 23:32:42 -0400

    Implemented EnterKey, no functionality

---
+++ client/src/components/EnterKey.jsx
@@ -0,0 +1,15 @@
+const EnterKey = () => {
+  return (
+    <>
+      <button
+        className={
+          "flex font-bold h-[58px] items-center justify-center text-1xl bg-gray-200 rounded-md grow border border-white hover:bg-transparent hover:border-gray-200"
+        }
+      >
+        Enter
+      </button>
+    </>
+  );
+};
+
+export default EnterKey;
