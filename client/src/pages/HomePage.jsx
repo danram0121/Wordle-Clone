7ee75a8ced59b4ef7205efa63976a93e224f85e2
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-27 23:10:45 -0400

    chore: Refactor homepage layout to use flexbox for centering Wordle game component

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,10 +3,8 @@ import Wordle from "./Wordle";
 const HomePage = () => {
   return (
     <>
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-center">
-          <Wordle />
-        </div>
+      <div className="flex justify-center">
+        <Wordle />
       </div>
     </>
   );
