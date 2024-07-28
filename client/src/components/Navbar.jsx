1486dc7b96658d6a7e6f17e95fbcf16d3f5b3293
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-27 20:24:12 -0400

    refactor: layed out navbar using tailwind classes and react-icons

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import { FaBars, FaRegLightbulb, FaRegQuestionCircle } from "react-icons/fa";
+import { FaChartSimple, FaGear } from "react-icons/fa6";
 
 const Navbar = () => {
   return (
     <>
-      <div>Navbar</div>
+      <nav className="border-b">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="flex h-20 items-center justify-between">
+            <div className="flex flex-1 items-center md:items-stretch md:justify-start">
+              <button className="text-3xl px-2 py-1 rounded-md hover:bg-gray-200">
+                <FaBars />
+              </button>
+            </div>
+            <div className="flex flex-1 items-center md:items-stretch md:justify-end gap-">
+              <button className="text-3xl px-2 py-1 rounded-md  hover:bg-gray-200">
+                <FaRegLightbulb />
+              </button>
+              <button className="text-3xl px-2 py-1 rounded-md hover:bg-gray-200">
+                <FaChartSimple />
+              </button>
+              <button className="text-3xl px-2 py-1 rounded-md hover:bg-gray-200">
+                <FaRegQuestionCircle />
+              </button>
+              <button className="text-3xl px-2 py-1 rounded-md hover:bg-gray-200">
+                <FaGear />
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
     </>
   );
 };
