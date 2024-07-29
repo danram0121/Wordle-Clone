0e88b7c94210350a3e9aa012e76ab267320cb716
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-28 22:14:38 -0400

    updated startbutton component to set the initial state or "start" the game

--- client/src/components/StartButton.jsx
+++ client/src/components/StartButton.jsx
@@ -1,15 +1,42 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
+import { GameStateContext } from "../context/GameStateContext";
 
 const StartButton = () => {
+  const { updateGameState } = useContext(GameStateContext);
+
+  const navigate = useNavigate();
+  const gameState = {
+    boardState: ["", "", "", "", "", ""],
+    currentRowIndex: 0,
+    word: "SLAIN",
+    puzzleId: "1",
+    legacyStats: {
+      currentStreak: 0,
+      gamesPlayed: 0,
+      gamesWon: 0,
+      longestStreak: 0,
+    },
+  };
+
+  const handleStart = (e) => {
+    e.preventDefault();
+
+    updateGameState(gameState);
+
+    navigate("/wordle");
+  };
+
   return (
     <>
       <section className="mb-6">
         <div className="max-w-sm mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
-          <Link to="/wordle">
-            <button className="mb-2 px-12 py-4 bg-black text-white text-lg rounded-full">
-              Start
-            </button>
-          </Link>
+          <button
+            className="mb-2 px-12 py-4 bg-black text-white text-lg rounded-full"
+            onClick={handleStart}
+          >
+            Start
+          </button>
         </div>
       </section>
     </>
