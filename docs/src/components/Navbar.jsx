e1a2e0d14edeb46d160533af5642e3ac23365361
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-29 14:40:08 -0400

    Add button to navigate to home screen

--- docs/src/components/Navbar.jsx
+++ docs/src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { FaBars, FaRegLightbulb, FaRegQuestionCircle } from "react-icons/fa";
 import { FaChartSimple, FaGear } from "react-icons/fa6";
 
@@ -7,22 +8,27 @@ const Navbar = () => {
       <nav className="border-b">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="flex h-20 items-center justify-between">
-            <div className="flex flex-1 items-center md:items-stretch md:justify-start">
+            <div className="flex flex-1 items-center md:items-stretch md:justify-start xxsm:h-10">
               <button className="text-3xl px-2 py-1 rounded-md hover:bg-gray-200">
                 <FaBars />
               </button>
             </div>
-            <div className="flex flex-1 items-center md:items-stretch md:justify-end gap-">
-              <button className="text-3xl px-2 py-1 rounded-md  hover:bg-gray-200">
+            <Link to={"/"}>
+              <button className="sm:text-sm xxsm:text-sm px-2 py-1 rounded-md  hover:bg-gray-200 bg-black text-white xxsm:h-10">
+                New Game
+              </button>
+            </Link>
+            <div className="flex flex-1 items-center md:items-stretch xxsm:justify-end xxsm:h-10">
+              <button className="text-3xl px-2 py-1 rounded-md  hover:bg-gray-200 xxsm:hidden sm:block">
                 <FaRegLightbulb />
               </button>
-              <button className="text-3xl px-2 py-1 rounded-md hover:bg-gray-200">
+              <button className="text-3xl px-2 py-1 rounded-md hover:bg-gray-200 xxsm:hidden sm:block">
                 <FaChartSimple />
               </button>
-              <button className="text-3xl px-2 py-1 rounded-md hover:bg-gray-200">
+              <button className="text-3xl px-2 py-1 rounded-md hover:bg-gray-200  xxsm:hidden sm:block">
                 <FaRegQuestionCircle />
               </button>
-              <button className="text-3xl px-2 py-1 rounded-md hover:bg-gray-200">
+              <button className="text-3xl px-2 py-1 rounded-md hover:bg-gray-200  xxsm:hidden sm:block">
                 <FaGear />
               </button>
             </div>
