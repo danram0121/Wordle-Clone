7c0371b5eacd00eaab0ade4b9a659cd24308b157
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-29 01:01:14 -0400

    update enter key to handle "submitting guess" and verifies win

--- client/src/components/EnterKey.jsx
+++ client/src/components/EnterKey.jsx
@@ -1,10 +1,60 @@
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { GameStateContext } from "../context/GameStateContext";
+import Confetti from "react-confetti";
+
 const EnterKey = () => {
+  const [winState, setWinState] = useState("");
+  const [loseState, setLoseState] = useState("");
+  const { currentState, updateGameState } = useContext(GameStateContext);
+  const rowIndex = currentState.currentRowIndex;
+  const currentGuess = currentState.boardState[rowIndex];
+  const rowLength = currentState.boardState[rowIndex].length;
+  const win = currentGuess === currentState.word;
+
+  const navigate = useNavigate();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    updateGameState((prevState) => {
+      const currentRowIndex = prevState.currentRowIndex;
+
+      // Check if the current row is less than 5 and the row length is 5
+      if (currentRowIndex < 5 && rowLength === 5) {
+        if (win) {
+          // Win condition
+          setWinState(true);
+          setTimeout(() => {
+            navigate("/");
+          }, 8000);
+        }
+        return {
+          ...prevState,
+          currentRowIndex: currentRowIndex + 1,
+        };
+      }
+
+      if (currentRowIndex === 5) {
+        // Game Over no more guesses remaining loss condition
+        setLoseState(true);
+        setTimeout(() => {
+          navigate("/");
+        }, 3000);
+      }
+
+      return {
+        ...prevState,
+      };
+    });
+  };
+
   return (
     <>
+      {winState ? <Confetti /> : null}
       <button
-        className={
-          "flex font-bold h-[58px] items-center justify-center text-1xl bg-gray-200 rounded-md grow border border-white hover:bg-transparent hover:border-gray-200"
-        }
+        className="flex font-bold h-[58px] items-center justify-center text-1xl bg-gray-200 rounded-md grow border border-white hover:bg-transparent hover:border-gray-200"
+        onClick={handleClick}
       >
         Enter
       </button>
