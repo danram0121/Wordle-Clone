2d9e6e89bb69273d583d4cab3f6110b2747740bc
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-28 22:25:56 -0400

    updated backspace to update current rows state slicing the array by 1 every click until the array is length 0

--- client/src/components/BackSpaceKey.jsx
+++ client/src/components/BackSpaceKey.jsx
@@ -1,9 +1,38 @@
+import { useContext, useMemo } from "react";
+import { GameStateContext } from "../context/GameStateContext";
 import { IoBackspaceOutline } from "react-icons/io5";
 
 const BackSpaceKey = () => {
+  const { updateGameState } = useContext(GameStateContext);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    updateGameState((prevState) => {
+      const currentRowIndex = prevState.currentRowIndex;
+      const currentRowContent = prevState.boardState[currentRowIndex];
+
+      if (currentRowContent.length > 0) {
+        const newBoardState = [...prevState.boardState];
+        newBoardState[currentRowIndex] = currentRowContent.slice(0, -1);
+
+        return {
+          ...prevState,
+          boardState: newBoardState,
+        };
+      }
+      return {
+        ...prevState,
+      };
+    });
+  };
+
   return (
     <>
-      <button className="flex h-[58px] items-center justify-center text-3xl bg-gray-200 rounded-md grow border border-white hover:bg-transparent hover:border-gray-200">
+      <button
+        className="flex h-[58px] items-center justify-center text-3xl bg-gray-200 rounded-md grow border border-white hover:bg-transparent hover:border-gray-200"
+        onClick={handleClick}
+      >
         <IoBackspaceOutline />
       </button>
     </>
