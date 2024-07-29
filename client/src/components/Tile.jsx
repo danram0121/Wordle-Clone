e5c74d9d7fa8e306e935fc796c8183b067ffc861
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-28 22:20:18 -0400

    updated tile to use game state context to dynamically change styling

--- client/src/components/Tile.jsx
+++ client/src/components/Tile.jsx
@@ -1,8 +1,32 @@
-const Tile = (prop) => {
+import { useContext, useMemo } from "react";
+import { GameStateContext } from "../context/GameStateContext";
+
+const theme = {
+  empty: "border-2 border-gray-300",
+  pending: "border-2 border-[#787c7e]",
+  absent: "bg-[#787c7e] text-white",
+  present: "bg-[#c9b458] text-white",
+  correct: "bg-[#58a351] text-white",
+};
+
+const Tile = ({ value, index, state }) => {
+  const { currentState } = useContext(GameStateContext);
+  const keyWord = currentState.word;
+
+  const tileState = useMemo(() => {
+    if (value === " ") return theme.empty;
+    if (state === "pending") return theme.pending;
+    if (value === keyWord[index]) return theme.correct;
+    if (keyWord.includes(value)) return theme.present;
+    return theme.absent;
+  }, [value, index, state]);
+
   return (
     <>
-      <div className="flex size-[62px] items-center justify-center text-3xl font-bold border-2 border-gray-300">
-        {prop.letter}
+      <div
+        className={`flex size-[62px] items-center justify-center text-3xl font-bold ${tileState}`}
+      >
+        {value}
       </div>
     </>
   );
