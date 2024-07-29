af54573d0e64516547814d557ca5c09df41aa2f3
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-29 16:01:39 -0400

    minor changes to style and updated content

--- docs/src/components/Hero.jsx
+++ docs/src/components/Hero.jsx
@@ -3,11 +3,16 @@ const Hero = () => {
     <>
       <section className="">
         <div className="max-w-7xl sm:px-6 lg:px-8 flex flex-col items-center">
-          <div className="text-center">
-            <h1 className="text-4xl font-extrabold  sm:text-5xl md:text-6xl">
-              Welcome
+          <div className="text-center mb-2">
+            <h1 className="text-4xl font-extrabold  sm:text-5xl md:text-6xl xxsm:text-3xl sm:mb-6 xxsm:mb-2">
+              Welcome to Wordle
             </h1>
-            <p className="text-4xl">Good luck!</p>
+            <p className="sm:text-5xl xxsm:text-3xl sm:mb-2 xxsm:mb-2">
+              Good luck!
+            </p>
+            <p className="sm:text-lg xxsm:text-xs xxsm:font-light">
+              Note: words don&apos;t have repeating letters
+            </p>
           </div>
         </div>
       </section>
